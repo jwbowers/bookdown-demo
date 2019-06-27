6cafb1c3e0fd3582f9820e9a2f349818c3762d33
Jake Bowers <jake@jakebowers.org>
2019-06-27 10:10:02 -0700

    Local changes to enable building and deployment with out Travis

--- _deploy.sh
+++ _deploy.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+# [ -z "${GITHUB_PAT}" ] && exit 0
+# [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "xie@yihui.name"
-git config --global user.name "Yihui Xie"
+# git config --global user.email "xie@yihui.name"
+# git config --global user.name "Yihui Xie"
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
+rm -rf book-output
+git clone -b gh-pages git@github.com:jwbowers/bookdown-demo.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
 git commit -m"Update the book" || true
 git push -q origin gh-pages
+git checkout master
